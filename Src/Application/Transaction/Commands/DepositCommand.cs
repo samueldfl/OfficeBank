d92a03b941caac34b233c5961d6bec85dc794549
Samuel Duarte <samueldflima@proton.me>
2024-04-26 22:31:35 -0300

    Added Deposit Command

---
+++ Src/Application/Transaction/Commands/DepositCommand.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Shared.Commands;
+using Domain.Shared.ValidationStates;
+
+namespace Application.Transaction.Commands;
+
+public sealed record DepositCommand(string ToAccountId, decimal Amount) : ICommand
+{
+    public ValidationState Validate()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Src/Application/Transaction/Handlers/DepositCommandHandler.cs
@@ -0,0 +1,80 @@
+﻿using Application.Shared.CommandHandlers;
+using Application.Shared.ResultStates;
+using Application.Transaction.Commands;
+using Domain.Account.Models;
+using Domain.Account.Repositories;
+using Domain.Payment.Models;
+using Domain.Shared.ValidationStates;
+using Domain.Transaction.Repositories;
+using Infra.Shared.Database.UnitOfWork;
+
+namespace Application.Transaction.Handlers;
+
+public sealed class DepositCommandHandler : ICommandHandler<DepositCommand>
+{
+    private readonly IAccountRepository _accountRepository;
+
+    private readonly ITransactionRepository _transactionRepository;
+
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DepositCommandHandler(
+        IAccountRepository accountRepository,
+        ITransactionRepository transactionRepository,
+        IUnitOfWork unitOfWork
+    )
+    {
+        _accountRepository = accountRepository;
+        _transactionRepository = transactionRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<RootResult> HandleAsync(
+        DepositCommand command,
+        CancellationToken cancellationToken
+    )
+    {
+        ValidationState validation = command.Validate();
+
+        if (validation is FailureValidationState)
+        {
+            return new RootBadRequestResult();
+        }
+
+        try
+        {
+            AccountModel toAccount = await _accountRepository.GetAccountAsNoTrackingAsync(
+                account => account.Id == Guid.Parse(command.ToAccountId),
+                cancellationToken
+            );
+
+            TransactionModel lastTransaction =
+                await _transactionRepository.GetLastAccountTransactionAsNoTrackingAsync(
+                    account => account.Id == Guid.Parse(command.ToAccountId),
+                    cancellationToken
+                );
+
+            TransactionModel depositTransaction =
+                new()
+                {
+                    LastBalance = lastTransaction.Balance,
+                    Balance = lastTransaction.Balance + command.Amount,
+                    BalanceDiff = command.Amount,
+                    AccountId = toAccount.Id,
+                };
+
+            await _transactionRepository.CreateTransactionAsync(
+                depositTransaction,
+                cancellationToken
+            );
+
+            await _unitOfWork.CommitAsync(cancellationToken);
+
+            return new RootOkResult();
+        }
+        catch (Exception e)
+        {
+            return new RootFailureResult() { Body = e.Message };
+        }
+    }
+}
